41258ec413a800018e9ea9c0979ce84f6c4f5339
fcarrascomarin <fcarrascomarin95@gmail.com>
2026-06-29 11:25:39 -0400

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import Navbar from "./components/Navbar.jsx";
 import Hero from "./components/Hero.jsx";
 import Noticias from "./components/Noticias.jsx";
+import Plataforma from "./components/Plataforma.jsx";
+import CasosTransformacion from "./components/CasosTransformacion.jsx";
 import Trayectoria from "./components/Trayectoria.jsx";
 import Contacto from "./components/Contacto.jsx";
 import Footer from "./components/Footer.jsx";
@@ -14,6 +16,7 @@ export default function App() {
         <Hero />
         <Noticias />
         <Plataforma />
+        <CasosTransformacion />
         <Trayectoria />
         <Contacto />
       </main>
